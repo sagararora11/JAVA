d4602e5594b4d3e838ad9b6faaf98ba960b91ebb
sagararora11 <sagar18csu186@ncuindia.edu>
2019-01-12 01:48:59 +0530

    excercise answer 1

---
+++ bodmas.java
@@ -0,0 +1,16 @@
+class bodmas
+{
+public static void main(String args[])
+{
+	int add,sub,mult,x=12,y=6;
+	float div;
+	div=x/y;
+	add=x+y;
+	sub=x-y;
+	mult=x*y;
+	System.out.print("division:" +div);
+	System.out.print("addition:" +add);
+	System.out.print("subtraction:" +sub);
+	System.out.print("multiplication:" +mult);
+}
+}
